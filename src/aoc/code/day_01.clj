341ff5f733989ce04e543a364538a416b4aecf51
Yannick Lehnhausen <yannick@lehnhausen.dev>
2023-12-03 18:21:38 +0100

    refactoring

--- src/aoc/code/day_01.clj
+++ src/aoc/code/day_01.clj
@@ -2,10 +2,9 @@
 (ns aoc.code.day_01
   (:gen-class)
   (:require
-   [aoc.helpers :refer [format-input]]
-   [clojure.string :as str]))
+   [aoc.helpers :refer [format-input]]))
 
-; --- Day 1: Trebuchet?! ---
+;# --- Day 1: Trebuchet?! ---
 
 ; Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
 
@@ -20,12 +19,12 @@
 ; The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
 
 ; For example:
-
+; ```
 ; 1abc2
 ; pqr3stu8vwx
 ; a1b2c3d4e5f
 ; treb7uchet
-
+; ```
 ; In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 
 ; Consider your entire calibration document. What is the sum of all of the calibration values?
@@ -45,12 +44,12 @@
 {:nextjournal.clerk/visibility {:result :show}}
 (solution-1 "input/day_01/input.txt")
 
-; --- Part Two ---
+;# --- Part Two ---
 ;
 ; Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
 ;
 ; Equipped with this new information, you now need to find the real first and last digit on each line. For example:
-;
+; ```
 ; two1nine
 ; eightwothree
 ; abcone2threexyz
@@ -58,7 +57,7 @@
 ; 4nineeightseven2
 ; zoneight234
 ; 7pqrstsixteen
-;
+; ```
 ; In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
 ;
 ; What is the sum of all of the calibration values?
